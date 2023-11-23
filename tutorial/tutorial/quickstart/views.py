6bba206eeddc4228daa5983570b26c6f1040703c
anandbhere <anandbhere46@gmail.com>
2023-11-23 22:40:19 +0530

    First commit  Serilalisers created

---
+++ tutorial/tutorial/quickstart/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Superusers  Andy pw-Andy$2023  admin pw-password123
+
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+
+
